af59c1a992a0469096be9081dd6a3b0807bfc3b4
Tatjana <zev333@inbox.lv>
2012-07-22 23:02:01 +0300

    Started something. Just name of my class

---
+++ Cube3Project/Cube3/Cube3/Cube3/CubeMain.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Cube3
+{
+    public class CubeMain
+    {
+        private int [][] aLayerRotation;
+        private int [][] bLayerRotation;
+        private int [][] cLayerRotation;
+
+        CubeMain()
+        {
+
+        }
+    }
+}
